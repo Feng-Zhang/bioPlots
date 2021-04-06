ab60b5b421debdc7b1c0c2d89e4a788cf6ae13e7
章峰 <fengzhang0709@hotmail.com>
2021-04-06 19:09:02 +0800

    feat:not finish

--- R/plotBarSdSig.R
+++ R/plotBarSdSig.R
@@ -1,6 +1,7 @@
 ##' @title plotBarSdSig
 ##' @description This function would generate a list including a countData and colData as input data for testing
-##' @param mydata a dataframe with two column of group and phe
+##' @param data Default dataset to use for plot.
+##' @param mapping Default list of aesthetic mappings to use for plot
 ##' @return a ggplot2 object
 ##' @examples
 ##' library(ggplot2);library(plyr);library(ggpubr)
@@ -12,20 +13,20 @@
 ##' @importFrom stats sd
 ##' @export
 
-plotBarSdSig <- function(mydata) {
-  #mydata=mpg
-  groupName = unique(mydata$group)
-  summ = ddply(mydata,"group",summarise,mean=mean(phe),sd=sd(phe),n=length(phe),se=sd/sqrt(n),max=max(phe)) #对各组计算mean和sd等
-  row.names(summ)=summ$group
-  p = ggplot(mydata, aes(group, phe)) + geom_bar(stat="identity", position="dodge", width=.5)+ #画柱状图
-    geom_errorbar(data = summ, aes(x = group, y=max,ymin = max, ymax = max + sd, width=.2),  position = position_dodge(width=.8))+ #画标准差
-    geom_signif(comparisons=list(c(groupName[1],groupName[2]),
-                                 c(groupName[1],groupName[3]),
-                                 c(groupName[2],groupName[3])),
-                y_position=c(summ[groupName,"max"] ),tip_length = 0.02, vjust=0.2, map_signif_level=TRUE) #画显著星号
-
-
-  p = p+theme_bw() +theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank(),panel.border = element_blank(),axis.line = element_line(colour = "black")) #去除背景色、格子和边框
-
-  return(p)
+plotBarSdSig <- function(data,mapping) {
+  # #mydata=mpg
+  # groupName = unique(mydata[,as.name(group)])
+  # summ = ddply(mydata,group,summarise,mean=mean(phe),sd=sd(phe),n=length(phe),se=sd/sqrt(n),max=max(phe)) #对各组计算mean和sd等
+  # row.names(summ)=summ$group
+  # p = ggplot(mydata, aes(group, phe)) + geom_bar(stat="identity", position="dodge", width=.5)+ #画柱状图
+  #   geom_errorbar(data = summ, aes(x = group, y=max,ymin = max, ymax = max + sd, width=.2),  position = position_dodge(width=.8))+ #画标准差
+  #   geom_signif(comparisons=list(c(groupName[1],groupName[2]),
+  #                                c(groupName[1],groupName[3]),
+  #                                c(groupName[2],groupName[3])),
+  #               y_position=c(summ[groupName,"max"] ),tip_length = 0.02, vjust=0.2, map_signif_level=TRUE) #画显著星号
+  #
+  #
+  # p = p+theme_bw() +theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank(),panel.border = element_blank(),axis.line = element_line(colour = "black")) #去除背景色、格子和边框
+  #
+  # return(p)
 }
--- R/plotLmCor.R
+++ R/plotLmCor.R
@@ -1,18 +1,18 @@
 ##' @title plotLmCor
 ##' @description This function would generate a list including a countData and colData as input data for testing
-##' @param mydata a dataframe with two column of x and y
+##' @param data Default dataset to use for plot.
+##' @param mapping Default list of aesthetic mappings to use for plot
 ##' @return a ggplot2 object
 ##' @examples
 ##' require(ggplot2)
-##' mydata = mpg[,8:9];colnames(mydata)=c("x","y")
-##' plotLmCor(mydata)
+##' print(plotLmCor(mpg,aes(cty,hwy)))
 ##' @import ggplot2
 ##' @importFrom ggpubr stat_cor
 ##' @importFrom stats lm
 ##' @export
 
-plotLmCor <- function(mydata) {
-  p<-ggplot(mydata,aes(x,y))+geom_point()+
+plotLmCor <- function(data,mapping) {
+  p<-ggplot(data,mapping)+geom_point()+
     stat_smooth(method=lm) +
     stat_cor(method = "pearson")
   return(p)

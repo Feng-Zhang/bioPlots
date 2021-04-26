0db69aa132d2610c1279404b2cc29c99405a7c39
章峰 <fengzhang0709@hotmail.com>
2021-04-26 17:10:59 +0800

    feat:修改plotBarSdSig，使用准引用写法

--- R/plotLmCor.R
+++ R/plotLmCor.R
@@ -17,3 +17,24 @@ plotLmCor <- function(data,mapping) {
     stat_cor(method = "pearson")
   return(p)
 }
+
+##' @title plotLmCor2
+##' @description This function would generate a list including a countData and colData as input data for testing
+##' @param data Default dataset to use for plot.
+##' @param x Default variable from data for aesthetic mappings to use for plot
+##' @param y Default variable from data for aesthetic mappings to use for plot
+##' @return a ggplot2 object
+##' @examples
+##' require(ggplot2)
+##' print(plotLmCor2(mpg,cty,hwy))
+##' @import ggplot2
+##' @importFrom ggpubr stat_cor
+##' @importFrom stats lm
+##' @export
+plotLmCor2 <- function(data,x,y){
+  p <- ggplot(data,aes_(substitute(x),substitute(y)))+
+    geom_point()+
+    stat_smooth(method=lm) +
+    stat_cor(method = "pearson")
+  return(p)
+}

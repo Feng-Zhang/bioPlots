8eaecb53fda1c4e889d9ab1de7b1d18df18133fb
章峰 <fengzhang0709@hotmail.com>
2021-05-03 15:52:37 +0800

    完成基本绘图函数框架
    commit 0db69aa132d2610c1279404b2cc29c99405a7c39
    Author: 章峰 <fengzhang0709@hotmail.com>
    Date:   Mon Apr 26 17:10:59 2021 +0800
    
        feat:修改plotBarSdSig，使用准引用写法
    
    commit c184841969489b522402c1e7188ef1f6d4b7f3b6
    Author: 章峰 <fengzhang0709@hotmail.com>
    Date:   Thu Apr 8 14:13:11 2021 +0800
    
        fix:解决pull冲突
    
    commit 28f8daa6c64fcbbe7f8c742da45df4666dee1885
    Merge: 6e3aa22 c7f703b
    Author: 章峰 <fengzhang0709@hotmail.com>
    Date:   Thu Apr 8 14:04:31 2021 +0800
    
        Merge remote-tracking branch 'origin/dev' into dev
    
    commit 6e3aa220568d613f0e7bd2ac233554223ea7eb50
    Author: 章峰 <fengzhang0709@hotmail.com>
    Date:   Thu Apr 8 09:50:34 2021 +0800
    
        feat:add overallGSEA
    
    commit c7f703b7c88f656d090d66f5d4b9cc2da46744a6
    Author: 章峰 <fengzhang0709@hotmail.com>
    Date:   Thu Apr 8 09:50:34 2021 +0800
    
        feat:add overallGSEA
    
    commit ab60b5b421debdc7b1c0c2d89e4a788cf6ae13e7
    Author: 章峰 <fengzhang0709@hotmail.com>
    Date:   Tue Apr 6 19:09:02 2021 +0800
    
        feat:not finish
    
    commit 6cf6f0999966890879273b6a0f72f67bf80b6aec
    Author: 章峰 <fengzhang0709@hotmail.com>
    Date:   Mon Mar 1 13:17:20 2021 +0800
    
        perf:增加依赖的包
    
    commit d3bfc893a03978938c1cb24f90adbff7dcfa8e7c
    Author: 章峰 <fengzhang0709@hotmail.com>
    Date:   Sun Jan 10 19:43:02 2021 +0800
    
        perf:check通不过
    
    commit df56ae6eb03d16d883de1c016ed9bf347af21249
    Author: Feng-Zhang <fengzhang0709@hotmail.com>
    Date:   Sun Jan 10 14:58:38 2021 +0800
    
        feat(R):增加plotLmCor.R

--- R/plotBarSdSig.R
+++ R/plotBarSdSig.R
@@ -1,34 +1,56 @@
 ##' @title plotBarSdSig
 ##' @description This function would generate a list including a countData and colData as input data for testing
-##' @param mydata a dataframe with two column of group and phe
+##' @param data Default dataset to use for plot.
+##' @param x Default variable from data for aesthetic mappings to use for plot
+##' @param y Default variable from data for aesthetic mappings to use for plot
+##' @param rowOrder the order of x-axis
+##' @param comparisons A list of length-2 vectors. The entries in the vector are either the names of 2 values on the x-axis or the 2 integers that correspond to the index of the columns of interest.
+##' @param y_position numeric vector with the y positions of the brackets
 ##' @return a ggplot2 object
 ##' @examples
 ##' \dontrun{
-##' require(ggplot2);require(plyr)
-##' mydata = mpg[,c("drv","hwy")];colnames(mydata)=c("group","phe")
-##' plotBarSdSig(mydata)
-##' }
+##' library(ggplot2);library(dplyr);library(ggsignif)
+##'
+##' compareGroup = list(c("f","4"),c("f","r"),c("4","r"))
+##' plotBarSdSig(mpg,drv,hwy,comparisons=compareGroup,y_position=TRUE)
+##'
+##' plotBarSdSig(mpg,drv,hwy,rowOrder=c("r","4","f"),
+##' comparisons=compareGroup,y_position=c(48.2,50.8,32.07))
+##'}
 ##' @import ggplot2
-##' @importFrom plyr ddply
+##' @import dplyr
 ##' @importFrom ggsignif geom_signif
 ##' @importFrom stats sd
 ##' @export
 
-plotBarSdSig <- function(mydata) {
-  colnames(mydata)=c("group","phe")
-  mydata=as.data.frame(mydata)
-  groupName =unique(mydata[,"group"])
-  summ = ddply(mydata,"group",summarise,mean=mean(phe),sd=sd(phe),n=length(phe),se=sd/sqrt(n),max=max(phe)) #对各组计算mean和sd等
-  row.names(summ)=summ$group
-  p = ggplot(mydata, aes(group, phe)) + geom_bar(stat="identity", position="dodge", width=.5)+ #画柱状图
-    geom_errorbar(data = summ, aes(x = group, y=max,ymin = max, ymax = max + sd, width=.2),  position = position_dodge(width=.8))+ #画标准差
-    geom_signif(comparisons=list(c(groupName[1],groupName[2]),
-                                 c(groupName[1],groupName[3]),
-                                 c(groupName[2],groupName[3])),
-                y_position=c(summ[groupName,"max"] ),tip_length = 0.02, vjust=0.2, map_signif_level=TRUE) #画显著星号
+plotBarSdSig <- function(data,x,y,rowOrder=c(),comparisons=NULL,y_position= NULL) {
+  topPos <- NULL
+  #data = data %>% arrange(match({{x}}, rowOrder))
+  summ = data %>% group_by({{x}}) %>%
+    summarise(mean=mean({{y}}),sd=sd({{y}}),n=length({{y}}),se=sd/sqrt(n),max=max({{y}}),topPos=max+sd) #对各组计算mean和sd等
 
+  if(is.null(comparisons) | is.null(y_position)){
+    y_position = NULL
+  } else if(is.logical(y_position)){
+    if(y_position){
+      y_position = sapply(comparisons,function(groupName){summ%>%filter({{x}} %in% groupName) %>% select(topPos) %>%max()})
+      print(paste0("The position of y axis for significant plotting: ",y_position))
+    }
+  }
+  # else if (!is.numeric(y_position)){
+  #   stop("y_postion should be TURE or FALSE or a numeric vector with same length as comparisons argument")
+  # }
 
+  if(is.null(rowOrder)) p = ggplot(data, aes({{x}}, {{y}})) else p=ggplot(data, aes(factor({{x}},levels = rowOrder), {{y}}))
+
+  p = p+ geom_bar(stat="identity", position="dodge", width=.5)+
+    geom_errorbar(data = summ, aes(x = {{x}}, y=max,ymin = max, ymax = max + sd, width=.2),  position = position_dodge(width=.8))+ #画标准差
+    geom_signif(comparisons=comparisons,
+                y_position=y_position,
+                tip_length = 0.02, vjust=0.2, map_signif_level=TRUE) #画显著星号
   p = p+theme_bw() +theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank(),panel.border = element_blank(),axis.line = element_line(colour = "black")) #去除背景色、格子和边框
 
   return(p)
 }
+
+
--- R/plotLmCor.R
+++ R/plotLmCor.R
@@ -1,18 +1,39 @@
 ##' @title plotLmCor
 ##' @description This function would generate a list including a countData and colData as input data for testing
-##' @param mydata a dataframe with two column of x and y
+##' @param data Default dataset to use for plot.
+##' @param mapping Default list of aesthetic mappings to use for plot
 ##' @return a ggplot2 object
 ##' @examples
 ##' require(ggplot2)
-##' mydata = mpg[,8:9];colnames(mydata)=c("x","y")
-##' plotLmCor(mydata)
+##' print(plotLmCor(mpg,aes(cty,hwy)))
 ##' @import ggplot2
 ##' @importFrom ggpubr stat_cor
 ##' @importFrom stats lm
 ##' @export
 
-plotLmCor <- function(mydata) {
-  p<-ggplot(mydata,aes_(~x,~y))+geom_point()+
+plotLmCor <- function(data,mapping) {
+  p<-ggplot(data,mapping)+geom_point()+
+    stat_smooth(method=lm) +
+    stat_cor(method = "pearson")
+  return(p)
+}
+
+##' @title plotLmCor2
+##' @description This function would generate a list including a countData and colData as input data for testing
+##' @param data Default dataset to use for plot.
+##' @param x Default variable from data for aesthetic mappings to use for plot
+##' @param y Default variable from data for aesthetic mappings to use for plot
+##' @return a ggplot2 object
+##' @examples
+##' require(ggplot2)
+##' print(plotLmCor2(mpg,cty,hwy))
+##' @import ggplot2
+##' @importFrom ggpubr stat_cor
+##' @importFrom stats lm
+##' @export
+plotLmCor2 <- function(data,x,y){
+  p <- ggplot(data,aes_(substitute(x),substitute(y)))+
+    geom_point()+
     stat_smooth(method=lm) +
     stat_cor(method = "pearson")
   return(p)
